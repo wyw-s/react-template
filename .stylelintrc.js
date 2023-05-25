11a10fe77ac876f0067deb588453232bec5e2e9c
王亚威 <wyw.s@foxmail.com>
2023-05-25 22:31:55 +0800

    feat(stylelint): 新增 postcss-less 依赖

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,8 +3,10 @@ const { getStylelintConfig } = require('@iceworks/spec');
 // getStylelintConfig(rule: 'common'|'rax'|'react'|'vue', customConfig?);
 module.exports = getStylelintConfig('react', {
   plugins: ['stylelint-less'],
+  customSyntax: 'postcss-less',
   rules: {
     'selector-max-id': 1,
-    'scss/at-rule-no-unknown': [true, { ignoreAtRules: ['/^custom-/'] }]
+    'scss/at-rule-no-unknown': [true, { ignoreAtRules: ['/^custom-/'] }],
+    "no-duplicate-selectors": null
   }
 });
